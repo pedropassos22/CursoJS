98cd6c7bc2fdf74acbbdf7ce1738cf01074e734b
Pedro Passos <100157780+pedropassos22@users.noreply.github.com>
2022-03-15 16:20:46 -0300

    aula 11

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -1,1 +1,12 @@
-alert('Com a nossa mensagem');
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite um número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+resultado = (num1 + num2);
+
+//console.log(num1 + num2);
+//console.log (resultado);
+
+//alert('A soma dos valores é igual a: ' + resultado); // metodo 1
+alert(`A soma dos valores é igual a ${resultado}`); // metodo 2
